1dd53302aaae026107f61d47ffe259125b03a981
nevergiveup10 <smolensky.p@gmail.com>
2020-06-07 23:46:41 +0300

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-function search(){
-    let search_string = document.getElementById('search_string').value;
-    let input = search_string.trim().replace("+", "%2B").replace("=", "%3D").replace("&", "%26").replace(" ", "+");
-    let search_url = 'https://www.google.com/search?q='
-    input = search_url+input;
-    window.location = input;
+function search() {
+  let search_string = document.getElementById("search_string").value;
+  let input = search_string
+    .trim()
+    .replace("+", "%2B")
+    .replace("=", "%3D")
+    .replace("&", "%26")
+    .replace(" ", "+");
+  let search_url = "https://www.google.com/search?q=";
+  input = search_url + input;
+  window.location = input;
 }
 
-
-document.getElementById('container').onclick=function(){      
-    document.getElementById('drop_menu').style.display='none';
-    document.getElementById('drop_account').style.display='none';
-    document.getElementById('drop_setting').style.display='none';
-}
+document.getElementById("container").onclick = function () {
+  document.getElementById("drop_menu").style.display = "none";
+  document.getElementById("drop_account").style.display = "none";
+  document.getElementById("drop_setting").style.display = "none";
+};
